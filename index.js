ae020789cf780a85e0d7aecc024cf2315faef0c6
Arthur Cezar <arthurcvm@hotmail.com>
2021-11-17 23:23:41 -0300

    refactor: implements cities endpoint & remove legacy code

--- index.js
+++ index.js
@@ -2,16 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const {
-  listarTarefaId,
-  listarTarefas,
-  cadastrarTarefa,
-  atualizarTarefa,
-  removerTarefa,
-  concluirTarefa
-} = require("./controllers/gerenciador-tarefas.js");
-
-const {finalizarCompra} = require("./controllers/mini-ecommerce.js");
+const {finalizarCompra, obterCidadesPorEstado} = require("./controllers/mini-ecommerce.js");
 const { application } = require("express");
 
 const app = express();
@@ -20,19 +11,7 @@ const port = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/gerenciador-tarefas", listarTarefas);
-
-app.get("/gerenciador-tarefas/:id", listarTarefaId);
-
-app.post("/gerenciador-tarefas", cadastrarTarefa);
-
-app.put("/gerenciador-tarefas/:id", atualizarTarefa);
-
-app.delete("/gerenciador-tarefas/:id", removerTarefa);
-
-app.put("/gerenciador-tarefas/:id/concluir", concluirTarefa);
-
-//mini-ecommerce
 app.post('/mini-ecommerce/checkout/finalizar-compra', finalizarCompra);
+app.get('/mini-ecommerce/estado/:siglaEstado/cidades', obterCidadesPorEstado);
 
 app.listen(port, () => console.log(`Servidor inicializado na porta ${port}`));
